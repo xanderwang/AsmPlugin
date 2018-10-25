d398e4184ffa005d2af58f524ea4614354f2de72
XanderWang <420640763@qq.com>
2018-10-25 22:32:39 +0800

    Initial commit

---
+++ xaop_app/src/main/java/com/xander/xaop/demo/MainActivity.java
@@ -0,0 +1,12 @@
+package com.xander.xaop.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(com.xander.demo.R.layout.activity_main);
+  }
+}
