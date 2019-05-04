7df6b6c78c33ce21824a12c3f24406b04f154e79
XanderWang <420640763@qq.com>
2019-05-04 23:07:49 +0800

    添加demo

--- xaop_plugin/src/main/groovy/com/xander/xaop/XaopPlugin.groovy
+++ xaop_plugin/src/main/groovy/com/xander/xaop/XaopPlugin.groovy
@@ -4,17 +4,14 @@ import com.android.build.gradle.AppExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-public class XaopPlugin implements Plugin<Project> {
+class XaopPlugin implements Plugin<Project> {
 
   void apply(Project project) {
-    System.out.println("========================")
-    System.out.println("Hello XaopPlugin!")
-    System.out.println("========================")
-    // method 1
-//    project.android.registerTransform(new CustormTransform())
-    // method 2
-    AppExtension appExtension = (AppExtension)project.getProperties().get("android")
-    appExtension.registerTransform(new CustormTransform(), Collections.EMPTY_LIST)
-
+    println "==========================="
+    println "Hello XaopPlugin!"
+    AppExtension appExtension = (AppExtension) project.getProperties().get("android")
+    appExtension.registerTransform(new CustormTransform(project), Collections.EMPTY_LIST)
+//    appExtension.registerTransform(new TestHunterExtension(project), Collections.EMPTY_LIST)
+    println "========================"
   }
 }
